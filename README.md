bf44edb152a66711cc5d82cb29f6ac61db3220dc
jamiejamiebobamie <jmccrory@vt.edu>
2018-12-09 21:16:58 -0800

    finished website w/ 3 features. see README.md

--- README.md
+++ README.md
@@ -7,13 +7,13 @@ Chosen Web API: Canvas
 Three Features:
         1.) Drawing (Spray Paint Can)
         2.) Animation (Ghost)
-        3.) Photo Manipulation ()
+        3.) Drawing Text (Win-Lose Status)
 
 The Canvas API has a host of features for enabling the visualization of graphics.
 
 There seems to be no limit to the uses of this API and what it can do, from rendering videos and pictures,
 to manipulating photos, to creating graphics for data visualization.
 
-In my opinion it is a great API  and very beginner friendly with lots of documentation online. (See above link.)
+In my opinion it is a great API and very beginner friendly with lots of documentation online. (See above link.)
 
 To run my site, please download the repo and open the index.html in any web browser. Some features of Canvas are only accessible with certain web browsers (namely Mozilla Firefox), but my site should run in any modern web browser.
--- script-spray.js
+++ script-spray.js
@@ -84,52 +84,28 @@ ctx.stroke();
 
 
 
+
+
+function draw3(text) {
+  var ctx = document.getElementById('canvas3').getContext('2d');
+  var ctx_style = document.getElementById('canvas3').style
+  ctx_style.zIndex = '8';
+  ctx.font = '48px serif';
+  ctx.fillText(text, 55, 90);
+}
+
+
+window.onload = function(){
+   setInterval(function(){
+       if (pressed == true){
+           text = "You won!";
+           draw3(text);
+       } else {
+           text = "You lost.";
+           draw3(text);
+       }
+   }, 5000);
+};
+
 draw();
-
 init();
-
-//var canvas = document.querySelector('.canvas'); // draw
-// var ctx = document.getElementById('canvas').getContext('2d'); //ghost
-
-
-
-//
-//
-// var moon = new Image();
-// function init() {
-//   moon.src = 'https://mdn.mozillademos.org/files/1443/Canvas_moon.png';
-//   window.requestAnimationFrame(draw);
-// }
-//
-// function draw() {
-//   var ctx = document.getElementById('canvas').getContext('2d');
-//   ctx.globalCompositeOperation = 'destination-over';
-//   ctx.clearRect(0, 0, 300, 300); // clear canvas
-//
-//   ctx.fillStyle = 'rgba(0, 0, 0, 0.4)';
-//   ctx.strokeStyle = 'rgba(0, 153, 255, 0.4)';
-//   ctx.save();
-//   ctx.translate(150, 150);
-//
-//   // Earth
-//   var time = new Date();
-//   ctx.rotate(((2 * Math.PI) / 60) * time.getSeconds() + ((2 * Math.PI) / 60000) * time.getMilliseconds());
-//   ctx.translate(105, 0);
-//
-//
-//   // Moon
-//   ctx.save();
-//   ctx.rotate(((2 * Math.PI) / 6) * time.getSeconds() + ((2 * Math.PI) / 6000) * time.getMilliseconds());
-//   ctx.translate(0, 28.5);
-//   ctx.drawImage(moon, -3.5, -3.5);
-//   ctx.restore();
-//
-//   ctx.restore();
-//
-//   ctx.beginPath();
-//   ctx.stroke();
-//
-//   window.requestAnimationFrame(draw);
-// }
-//
-// init();
